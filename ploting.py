4ade3f8b52cebaf02cc72ec422faeffd3f43a378
Eric Lee <ericlee911110@gmail.com>
2022-05-06 17:06:51 +0800

    apply the classifiers

--- ploting.py
+++ ploting.py
@@ -1,21 +1,119 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from sklearn.datasets import make_classification
+from matplotlib.colors import ListedColormap
+from sklearn.datasets import make_classification, make_moons, make_circles
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
-plt.figure(figsize=(8,8))
-plt.subplot(3,2,1)
-plt.title("Mess up the color")
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
-X1, Y1 = make_classification(
-    n_features=2, n_redundant=0, n_informative=1, n_clusters_per_class=1
-)
-arr=[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0]
-plt.scatter(X1[:,0], X1[:,1], marker="o", c=arr, s=25, edgecolor="k")
+h = 0.02
+figure = plt.figure(figsize=(27,9))
 
-plt.subplot(3,2,2)
-plt.title("This is what the color should be")
-plt.scatter(X1[:,0], X1[:,1], marker="o", c=Y1, s=25, edgecolor="k")
+X, y = make_classification(
+    n_features=2, n_redundant=0, n_informative=2, random_state=1, n_clusters_per_class=1
+) #n_feature is n_tuple. y represent their classes in (0,1,1,0,0,0)
+rng = np.random.RandomState(2)
+X += 2 * rng.uniform(size=X.shape) #more random?
+linearly_separable = (X, y)
 
+datasets = [
+    make_moons(noise=0.3, random_state=0),
+    make_circles(noise=0.2, factor=0.5, random_state=1),
+    linearly_separable
+]
+
+names = [
+    "Nearest Neighbors",
+    "Linear SVM",
+    "RBF SVM",
+    "Gaussian Process",
+    "Decision Tree",
+    "Random Forest",
+    "Neural Net",
+    "AdaBoost",
+    "Naive Bayes",
+    "QDA",
+]
+
+classifiers =[
+    KNeighborsClassifier(3),
+    SVC(kernel="linear", C=0.025),
+    SVC(gamma=2, C=1),
+    GaussianProcessClassifier(1.0 * RBF(1.0)),
+    DecisionTreeClassifier(max_depth=5),
+    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+    MLPClassifier(alpha=1, max_iter=1000),
+    AdaBoostClassifier(),
+    GaussianNB(),
+    QuadraticDiscriminantAnalysis()
+]
+
+i = 1
+for ds_cnt, ds in enumerate(datasets):
+    X, y = ds
+    X = StandardScaler().fit_transform(X) #Standardize
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.4, random_state=43 #train_size=0.6 We need a test_data to evaluate how arrcuate our model is
+    )
+    
+    x_min, x_max = X[:,0].min() - 0.5, X[:,0].max() + 0.5
+    y_min, y_max = X[:,1].min() - 0.5, X[:,1].max() + 0.5
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+    cm = plt.cm.RdBu
+    cm_bright = ListedColormap(["#FF0000", "#0000FF"])
+    ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+    if ds_cnt == 0:
+        ax.set_title("Input data")
+
+    ax.scatter(X_train[:,0], X_train[:,1], c=y_train, cmap=cm_bright, edgecolors="k")
+    ax.scatter(X_test[:,0], X_test[:,1], c=y_test, cmap=cm_bright, alpha=0.6, edgecolors="k")
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
+    ax.set_xticks(())
+    ax.set_yticks(())
+    print("firts", str(i))
+    i += 1
+    
+
+    for name, clf in zip(names, classifiers):
+        print("second")
+        ax = plt.subplot(len(datasets,), len(classifiers) + 1, i)
+        clf.fit(X_train, y_train)
+        score = clf.score(X_test, y_test)
+
+        if hasattr(clf, "decision_function"):
+            Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+        else:
+            Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+
+        Z = Z.reshape(xx.shape)
+        ax.contourf(xx, yy, Z, cmap=cm, alpha=0.8)
+
+        ax.scatter(
+                X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright, edgecolors="k")
+
+        ax.scatter(X_test[:, 0],
+                X_test[:, 1], c=y_test, cmap=cm_bright, edgecolors="k", alpha=0.6)
+
+        ax.set_xlim(x_min, x_max)
+        ax.set_ylim(y_min, y_max)
+        ax.set_xticks(())
+        ax.set_yticks(())
+        if ds_cnt ==0:
+            ax.set_title(name)
+        ax.text(x_max - 0.3, y_min + 0.3, ("%.2f" % score).lstrip("0"), size=15, horizontalalignment="right")
+        i += 1
+
+plt.tight_layout()
 plt.show()
-
---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+datasets=[
+    [[0,1,2,3],['a','b']],
+    [[10,11,12,13],['t','w']]
+]
+
+for i, r in datasets:
+    print(i)
